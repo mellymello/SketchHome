4fa7741d6bf04673ee6d7a3a1dea9b665a2eb395
calixte.melly <calixte.melly@heig-vd.ch>
2014-05-15 20:30:37 +0200

    export2

---
+++ SketchHome/src/features/ExportContent.java
@@ -0,0 +1,32 @@
+package features;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ExportContent 
+{
+	private JPanel content;
+	
+	public ExportContent (JPanel p)
+	{
+		content = p;
+	}
+	
+	
+	public void createPng(File selectedFile) 
+	{
+		BufferedImage img = new BufferedImage(content.getWidth(), content.getHeight(), BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2 = img.createGraphics();
+		content.paint(g2);
+		try{
+			ImageIO.write(img, "png", new File("test export.png"));
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
