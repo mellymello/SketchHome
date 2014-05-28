afdad87db27fbcf16d5ce2ee37f96d00d31d0a16
Simone_Righitto <simone.righitto@heig-vd.ch>
2014-05-28 11:47:29 +0200

    Objet Personalisé avec BufferedImage

--- SketchHome/src/drawableObjects/FurnitureCreationContent.java
+++ SketchHome/src/drawableObjects/FurnitureCreationContent.java
@@ -1,28 +1,39 @@
 package drawableObjects;
 
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class FurnitureCreationContent {
 
+
 	
-	private Ellipse2D tmpEllipse;
-	private ArrayList<Ellipse2D> ellipses;
+	private Vector<Ellipse2D> ellipses;
+	protected Vector<Point> points = new Vector<Point>();
+
+	public FurnitureCreationContent() {
+		ellipses = new Vector<Ellipse2D>();
 	
-	public FurnitureCreationContent(){
-		ellipses=new ArrayList<>();
-		tmpEllipse=null;
 	}
-	
-	public ArrayList<Ellipse2D> getEllipses(){
+
+	public Vector<Ellipse2D> getEllipses() {
 		return ellipses;
 	}
-	
-	public void setTmpEllipse(Ellipse2D tmpEllipse){
-		this.tmpEllipse=tmpEllipse;
-	}
-	
-	public void addEllipse(Ellipse2D finalEllipse){
+
+	public void addEllipse(Ellipse2D finalEllipse) {
 		ellipses.add(finalEllipse);
 	}
+
+	public Vector<Point> getPoints() {
+		return points;
+	}
+	
+	
+ public void clearContent(){
+	 ellipses.clear();
+	 points.clear();
+ }
 }
